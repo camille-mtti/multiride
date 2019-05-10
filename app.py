5093898677b67e36da2b2ccb6d0122178e10ce8d
camcam152 <camillemtti@hotmail.fr>
2019-05-10 16:48:42 +0200

    get journey from navitia + a bit of refacto

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 import journey
+import coordinate
 import json
 
 app = Flask(__name__)
@@ -19,10 +20,26 @@ if __name__ == '__main__':
 def get_geocode():
     content = request.data
     content = json.loads(content)["address"]
-    coord = journey.get_coordinates(content)
+    coord = coordinate.get_coordinates(content)
     return jsonify(lattitude=coord[0], longitude=coord[1])
 
 @app.route('/journey',methods=['POST'])
 def get_journey():
 
+    #loads content from request body
+    content = request.data
+    source = json.loads(content)["from"]
+    destination = json.loads(content)["to"]
+
+    #geocode coordinates
+    source_coord = coordinate.get_coordinates(source)
+    source_coord = coordinate.get_coordinates_string(source_coord)
+    print(source_coord)
+    #transform into string
+    dest_coord = coordinate.get_coordinates(destination)
+    dest_coord = coordinate.get_coordinates_string(dest_coord)
+
+    #request for journey
+    transport = journey.get_navitia_journey(source_coord, dest_coord)
+    return jsonify(transport)
 
--- journey.py
+++ journey.py
@@ -2,24 +2,16 @@ import requests
 import json
 
 
-# get coordinates of an address
-def get_coordinates(address):
-    #https://nominatim.openstreetmap.org/search?<params>
-    r = requests.get('https://nominatim.openstreetmap.org/search?format=json&q=' + address)
-    response = json.loads(r.text)
-    lat = response[0]['lat']
-    lon = response[0]['lon']
-    return [lat, lon]
 
 
-def get_navitia_journey(source,dest):
-    r = requests.get('https://api.navitia.io/v1/coverage/sandbox/journeys?from=' + source + '&to=' + dest,
+def get_navitia_journey(source, dest):
+    r = requests.get('https://api.navitia.io/v1/coverage/fr-idf/journeys?from=' + source + '&to=' + dest,
                      auth=('be72cc2c-e6eb-4628-9a88-b7c54746c61a', ''))
     response = json.loads(r.text)
     return response
 
 
-print(get_navitia_journey("48,834698,2.304648", "48.8744088,2.295508"))
+print(get_navitia_journey("2.304648;48.834698", "2.295508;48.8744088")['journeys'][0]['sections'][1]['display_informations'])
 
 #{"lattitude": "48.834698","longitude": "2.304648"}
 #{"lattitude":"48.8744088","longitude":"2.295508"}
