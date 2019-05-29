c3dbab1c570c5526888ed6ed749660435ac4c5f6
camcam152 <camillemtti@hotmail.fr>
2019-05-29 09:43:17 +0200

    adding edge object

---
+++ edge.py
@@ -0,0 +1,13 @@
+class Edge:
+    def __init__(self, src, dest, type, weigth):
+        self.src = src
+        self.dest = dest
+        self.weigth = weigth
+        self.type = type
+        self.avoidedEdges = []
+
+    def addAvoidedEdge(self, edge):
+        self.avoidedEdges.append(edge)
+
+
+
