adaf23e9fea6a57801795e046943c247c29a4b83
Henry Matheisen <henry.mthsn@gmail.com>
2019-05-29 16:20:51 +0200

    deleted spaces

--- journey.py
+++ journey.py
@@ -2,9 +2,6 @@ import requests
 import json
 from graph import Graph
 
-
-
-
 def get_navitia_journey(source, dest):
     r = requests.get('https://api.navitia.io/v1/coverage/fr-idf/journeys?from=' + source + '&to=' + dest,
                      auth=('be72cc2c-e6eb-4628-9a88-b7c54746c61a', ''))
