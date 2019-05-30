a6d2dccdd25578c60bb40c064ed8fc94607b616c
Henry Matheisen <henry.mthsn@gmail.com>
2019-05-30 16:44:28 +0200

    refractored

--- coordinate.py
+++ coordinate.py
@@ -3,14 +3,13 @@ import json
 
 # get coordinates of an address
 def get_coordinates(address):
-    # https://nominatim.openstreetmap.org/search?<params>
-    r = requests.get(
-        'https://nominatim.openstreetmap.org/search?format=json&q=' + address)
-    response = json.loads(r.text)
-    lat = response[0]['lat']
-    lon = response[0]['lon']
-    return [lat, lon]
+  # https://nominatim.openstreetmap.org/search?<params>
+  r = requests.get('https://nominatim.openstreetmap.org/search?format=json&q=' + address)
+  response = json.loads(r.text)
+  lat = response[0]['lat']
+  lon = response[0]['lon']
+  return [lat, lon]
 
 
 def get_coordinates_string(array):
-    return array[1]+";"+array[0]
+  return array[1]+";"+array[0]
--- journey.py
+++ journey.py
@@ -2,9 +2,10 @@ import requests
 import json
 from graph import Graph
 
+
 def get_navitia_journey(source, dest):
     r = requests.get('https://api.navitia.io/v1/coverage/fr-idf/journeys?from=' + source + '&to=' + dest,
-                     auth=('be72cc2c-e6eb-4628-9a88-b7c54746c61a', ''))
+      auth=('be72cc2c-e6eb-4628-9a88-b7c54746c61a', ''))
     response = json.loads(r.text)
     graph = Graph()
     graph.create_node_from_navitia(response)
