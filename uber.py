2df735aee919b8a648c379e240e84d779ce75aeb
Henry Matheisen <henry.mthsn@gmail.com>
2019-05-30 11:10:19 +0200

    changed entry coord format

--- uber.py
+++ uber.py
@@ -11,15 +11,17 @@ class Uber:
     self.session = Session(server_token=getenv('SERVER_TOKEN'))
     self.client = UberRidesClient(self.session)
 
-  def estimate_time(self, start_latitude, start_longitude, end_latitude, end_longitude):
+  def estimate_time(self, start_coord, end_coord):
     # Get prices and time estimates
     estimates = self.client.get_price_estimates(
-      start_latitude=start_latitude,
-      start_longitude=start_longitude,
-      end_latitude=end_latitude,
-      end_longitude=end_longitude,
+      start_latitude=start_coord.split(";")[1],
+      start_longitude=start_coord.split(";")[0],
+      end_latitude=end_coord.split(";")[1],
+      end_longitude=end_coord.split(";")[0],
       seat_count=2
     )
+
+    print(json.dumps(estimates.json.get("prices"), indent=2))
     
     # Search for UberX and return the average price and the time in a dict
     for estimate in estimates.json.get("prices"):
