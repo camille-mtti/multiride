0f8d11b0405bcf46fc75a308e95c81b8559b837e
unknown <camillemtti@gmail.com>
2019-05-31 16:19:52 +0200

    add duration in edges

--- edge.py
+++ edge.py
@@ -1,10 +1,13 @@
 class Edge:
-  def __init__(self, src, dest, type, weight):
+  def __init__(self, src, dest, type, weight, duration):
     self.src = src
     self.dest = dest
     self.weight = weight
     self.type = type
     self.avoidedEdges = []
+    self.line = None
+    self.duration = duration
+
 
   def addAvoidedEdge(self, edge):
     self.avoidedEdges.append(edge)
